01ec39af6bf1a74522fca495cecc747cc4918de6
Nodewarrior <monish161992@gmail.com>
2017-03-27 01:31:03 -0400

    Enabling functionality

--- index.js
+++ index.js
@@ -31,11 +31,12 @@ app.get('/webhook/', function(req, res) {
 })
 
 app.post('/webhook/', function(req, res) {
-	let messaging_events leteq.body.entry[0].messaging
+	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = messaging_events[i]
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
+			let text = event.message.text
 		sendText(sender, "Text echo: " + text.substrong(0,100))
 	}
   }
