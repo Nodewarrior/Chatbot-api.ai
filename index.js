3caa3be0a8d94e5f02b636e718b2b1dd6247d549
Nodewarrior <monish161992@gmail.com>
2017-03-27 03:32:59 -0400

    Adding apiai

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const request = require('request')
 
 const app = express();
 
+const apiai = require('apiai')
+const apiaiApp = apiai(APIAI_TOKEN)
+
+
+
+
 app.set('port', (process.env.PORT || 5000))
 
 //Allows to process data
@@ -27,6 +33,7 @@ app.get('/webhook/', function(req, res) {
 	res.send("Wrong token")
 })
 
+/*Handling messages*/
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
@@ -34,13 +41,14 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-		sendText(sender, "Text echo: " + text.substring(0,100))
+		//sendText(sender, "Text echo: " + text.substring(0,100))
+		sendText(event, text)
 	}
   }
   res.sendStatus(200)
 })
 
-function sendText(sender, text) {
+/*function sendText(sender, text) {
 	let messageData = {text : text}
 	request({
 		url: "https://graph.facebook.com/v2.8/me/messages",
@@ -58,6 +66,51 @@ function sendText(sender, text) {
 		}
 	})
 }
+*/
+
+function sendText(sender, text) {
+	let apiai = apiaiApp.textRequest(text, {
+		sessionId: 'tabby_cat'
+	})
+
+	apiai.on('response', (response) => {
+		console.log(response)
+		let aiText = response.result.fullfillment.speech;
+
+		request({
+		url: "https://graph.facebook.com/v2.8/me/messages",
+		qs: {access_token : token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message: {text, aiText}
+		}
+	}, (error, response) => {
+		if(error) {
+			console.log("sending error")
+		} else if(response.body.error) {
+			console.log("response body error")
+		}
+	}
+	})
+
+		apiai.on('error', (error) => {
+			console.log(error)
+		})
+
+		apiai.end()
+}
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(app.get('port'), function() {
 	console.log("running: port")
