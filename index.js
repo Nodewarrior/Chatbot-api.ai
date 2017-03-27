f8b9e99dba9389783ec4fce4fd94edde10ffdc44
Nodewarrior <monish161992@gmail.com>
2017-03-27 01:15:34 -0400

    Initing talkbot

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get('/', function(req, res) {
 	res.send("Welcome i am talkbot!")
 })
 
+token = "EAAStBoBcmPYBAGY8J6xZAEcAeFmBGrHk4HmpRAEqnf3IMOk4i4gwemSKTXDwAFB55Y5cp4QXQaw2xxRrWoy7lZA9oWZCDfJfai2NneZA4HO3wpUTK8seIOnbdvjb6lCZAvBwCnDZBo0DoCParHKXd2hk3WgRmbs4E4xFIDj6G1OAZDZD"
 //Facebook
 
 app.get('/webhook/', function(req, res) {
@@ -28,6 +29,37 @@ app.get('/webhook/', function(req, res) {
 	}
 	res.send("Wrong token")
 })
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events leteq.body.entry[0].messaging_events
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+		sendText(sender, "Text echo: " + text.substrong(0,100))
+	}
+  }
+  res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text : text}
+	request({
+		url: "https://graph.facebook.com/v2.8/me/messages",
+		qs: {access_token, token}
+		method: "POST"
+		json: {
+			receipt: {id: sender},
+			message: messageData
+		}
+	}, function(error, response, body) {
+		if(error) {
+			console.log("sending error")
+		} else if(response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
 
 app.listen(port, function() {
 	console.log("running: port" + port)
